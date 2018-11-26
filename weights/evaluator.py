6874e37c6fd39f69e0134227a5c08a6d48a6d419
Lindsey Gray <Lindsey.Gray@cern.ch>
2018-11-26 13:23:39 -0800

    example of analysis code with convenience features

--- weights/evaluator.py
+++ weights/evaluator.py
@@ -19,11 +19,11 @@ class denselookup(object):
     
     def __call__(self,*args):        
         inputs = list(args)
-        print inputs
+        #print inputs
         counts = None
-        print
+        #print
         for i in xrange(len(inputs)):
-            print i,type(inputs[i])
+            #print i,type(inputs[i])
             if isinstance(inputs[i], JaggedArray):
                 if counts is not None and set(counts) != set(inputs[i].counts) and len(counts) == len(inputs[i].counts):
                     if counts == -1:
@@ -32,7 +32,7 @@ class denselookup(object):
                         raise Exception('counts not uniform between all input jagged arrays!')
                 counts = inputs[i].counts
                 inputs[i] = inputs[i].flatten()
-                print type(inputs[i])
+                #print type(inputs[i])
             elif isinstance(inputs[i], np.ndarray):
                 counts = -1
         retval = self.__evaluate(*tuple(inputs))
@@ -40,8 +40,8 @@ class denselookup(object):
             if isinstance(arg, JaggedArray):
                 retval = JaggedArray.fromcounts(arg.counts,retval)
                 break
-        print retval
-        print
+        #print retval
+        #print
         return retval
                                                
     
@@ -53,16 +53,31 @@ class denselookup(object):
             indices.append(np.maximum(np.minimum(np.searchsorted(self.__axes, args[0], side='right')-1,self.__values.shape[0]-1),0))
         else:
             for dim in xrange(self.__dimension):
-                print self.__axes[dim], self.__values.shape
+                #print self.__axes[dim], self.__values.shape
                 indices.append(np.maximum(np.minimum(np.searchsorted(self.__axes[dim], args[dim], side='right')-1,self.__values.shape[len(self.__axes)-dim-1]-1),0))
         indices.reverse()
         return self.__values[tuple(indices)]
+    
+    def __repr__(self):
+        myrepr = "{} dimensional histogram with axes:\n".format(self.__dimension)
+        temp = "" 
+        if self.__dimension == 1:
+            temp = "\t1: {}\n".format(self.__axes)
+        else:
+            temp = "\t1: {}\n".format(self.__axes[0])
+        for idim in xrange(1,self.__dimension):
+            temp += "\t{}: {}\n".format(idim+1,self.__axes[idim])        
+        myrepr += temp
+        return myrepr
 
 class evaluator(object):
     def __init__(self,names,primitives):
         self.__functions = {}
         for key, idx in names.iteritems():
             self.__functions[key] = denselookup(*primitives[idx])
+            
+    def __dir__(self):
+        return self.__functions.keys()
         
     def __getitem__(self, key):
         return self.__functions[key]
